827de2350d32af1f957ed63c303d2dec40fe1626
Mark van den Brink <mark@vandenbr.ink>
2018-11-20 12:12:36 +0100

    Migrate to new editor version

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,31 @@ declare const PACKAGE_NAME: string;
 declare const PACKAGE_VERSION: string;
 
 /** Dependencies */
-import { ConditionCreateTemplates, Features, NodeBlock, PropertiesEditor, _, node } from "tripetto";
-import { IBlockProps } from "../types";
+import { NodeBlock, _, block, conditions, editor, slots } from "tripetto";
 
 /** Assets */
 import * as ICON from "../assets/icon.svg";
 
-@node(PACKAGE_NAME, _("Boilerplate v%1", PACKAGE_VERSION), PACKAGE_VERSION)
-export class Block extends NodeBlock<IBlockProps> {
-    /** Specifies an icon for the block. */
-    public static readonly Icon = ICON;
-
-    /** Map the block constructor to the type property. */
-    public readonly Type = Block;
-
-    /** Invoked when slots for this block should be defined. */
-    public OnSlots(): void {
-        // Prepare your data slots here...
+@block({
+    type: "node",
+    identifier: PACKAGE_NAME,
+    version: PACKAGE_VERSION,
+    icon: ICON,
+    label: _("Boilerplate v%1", PACKAGE_VERSION)
+})
+export class Example extends NodeBlock {
+    @slots
+    a(): void {
+        // Set your data slots here...
     }
 
-    /** Invoked when the properties editor is requested. */
-    public OnProperties(properties: PropertiesEditor): void {
-        Features.Static.General(properties);
-        Features.Name(this, properties);
+    @editor
+    b(): void {
+        // Implement the properties editor here...
     }
 
-    /** If your block supports conditions, return the create templates here. */
-    public OnConditions(): ConditionCreateTemplates {
-        return [];
+    @conditions
+    c(): void {
+        // Set conditions here...
     }
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,2 @@
-import { INodeBlock } from "@tripetto/map";
-
-/** Specifies the properties of the serialized block. */
-export interface IBlockProps extends INodeBlock {}
+/** Specifies the properties of the block. */
+export interface IBlockProps {}
